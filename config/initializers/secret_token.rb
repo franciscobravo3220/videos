189e667486a6bb0a3822b908d5ad51f0b31fda11
Francisco Bravo <franciscobravo3220@gmail.com>
2013-11-19 10:20:13 -0500

    Iniciando VideoCollector app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videos::Application.config.secret_key_base = '73d40dfabeb76d85f4efd84babb5d758f6f01a21c9753b4a829971b89965e99437ed0a37a95320049f453e5469403d52039e231c81cacebff330728a9efad9e5'
